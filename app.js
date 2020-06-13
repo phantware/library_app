8e6c7a95d72f87b5982a424d1718edf1f1a107f7
phantware <phantmoney2011@gmail.com>
2020-06-13 17:44:36 +0100

    development tooling successfully setup

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
+const debug = require('debug')('app');
+const morgan = require('morgan');
 
 const app = express();
-app.use(express.static(path.join(__dirname, "public")));
+app.use(morgan('tiny'));
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(
-  "/css",
-  express.static(path.join(__dirname, "/node_modules/bootstrap/dist/css"))
+  '/css',
+  express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')),
 );
 app.use(
-  "/js",
-  express.static(path.join(__dirname, "/node_modules/bootstrap/dist/js"))
+  '/js',
+  express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')),
 );
 app.use(
-  "/js",
-  express.static(path.join(__dirname, "/node_modules/jquery/dist"))
+  '/js',
+  express.static(path.join(__dirname, '/node_modules/jquery/dist')),
 );
 const port = process.env.Port || 5050;
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views/index.html"));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/index.html'));
 });
-app.listen(port, console.log(`app running at port ${port}.`));
+app.listen(port, debug(`app running at port ${port}.`));
